adef587b9398969eda4f83ceac30c783e46cc181
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2013-12-21 18:57:56 +0800

    Add a game socket.

--- es_core.go
+++ es_core.go
@@ -4,5 +4,14 @@ import "fmt"
 import "github.com/op/go-nanomsg"
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, game!\n")
+	
+	game_socket, err := nanomsg.NewSocket(nanomsg.AF_SP, nanomsg.BUS)
+        if err != nil {
+                panic(err)
+        }
+        _, err = game_socket.Connect("tcp://127.0.0.1:60206")
+        if err != nil {
+                panic(err)
+        }
 }

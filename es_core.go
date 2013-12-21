fe6d67a65e1ebe355635d57a83eb505f007197f1
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2013-12-21 23:36:47 +0800

    Init sdl.

--- es_core.go
+++ es_core.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
-import "github.com/op/go-nanomsg"
+import ("fmt"
+	"github.com/fire/go-sdl2/sdl"
+	"github.com/op/go-nanomsg")
 
 func main() {
 	fmt.Printf("Hello, game!\n")
+	
+	sdl.Init(sdl.INIT_EVERYTHING)
+	window := sdl.CreateWindow("es_core::SDL",
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		800,
+		600,
+		sdl.WINDOW_OPENGL | sdl.WINDOW_SHOWN)
+	if window == nil {
+		panic(fmt.Sprintf("SDL_CreateWindow failed: %s\n", sdl.GetError()))
+	}
 	
 	game_socket, err := nanomsg.NewSocket(nanomsg.AF_SP, nanomsg.BUS)
         if err != nil {
